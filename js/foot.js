31881107ee2978ced6e60eba1e1ea8d677e864d8
NattWara <nut_silenthacker@hotmail.com>
2020-08-07 10:24:53 +0700

    Fix bragi

--- js/foot.js
+++ js/foot.js
@@ -3328,27 +3328,14 @@ function StAllCalc()
 	// n_A_PassSkill3[32] = Musical Lesson Level
 	// n_A_PassSkill3[29] = Bard's INT
 	if(w){
-		// custom TalonRO Poem of Bragi after cast delay
+		// custom TalonRO Poem of Bragi after cast delay - base delay reduction for PVM was changed to 3% per bragi lv instead of 5%
 		// "we strongly think that the stacking of Bragi with items that grant ACD reduction is something to avoid" - GM Team, applied only to PvM
 		if (n_A_PassSkill3[45]) // PvP Mode
 			//n_tok[74] += w * 3 + 20 * Math.floor(w / 10) + n_A_PassSkill3[32] * 2 + Math.floor(n_A_PassSkill3[29] / 5);
-			
-			n_tok[74] += (
-				(w * 3)
-				+ ((Math.floor(w / 10)) x 20)
-				+ Math.floor(n_A_PassSkill3[29] / 5)
-				+ (n_A_PassSkill3[32] * 2)
-			);
-			
-			
+			n_tok[74] += ((w * 3) + ((Math.floor(w / 10)) x 20) + Math.floor(n_A_PassSkill3[29] / 5) + (n_A_PassSkill3[32] * 2));
 		else // PvM Mode
-			//n_tok[74]  = w * 3                           + n_A_PassSkill3[32] * 2 + Math.floor(n_A_PassSkill3[29] / 5); // Override all previous acd reduction bonus
-			
-			n_tok[74] = (
-				(w * 3)
-				//+ ((Math.floor(w / 10)) x 20)
-				+ Math.floor(n_A_PassSkill3[29] / 5)
-				+ (n_A_PassSkill3[32] * 2)
+			//n_tok[74]  = w * 3 + n_A_PassSkill3[32] * 2 + Math.floor(n_A_PassSkill3[29] / 5); // Override all previous acd reduction bonus
+			n_tok[74] = ((w * 3) /* + ((Math.floor(w / 10)) x 20) */ + Math.floor(n_A_PassSkill3[29] / 5) + (n_A_PassSkill3[32] * 2)
 			);
 	}
 	
